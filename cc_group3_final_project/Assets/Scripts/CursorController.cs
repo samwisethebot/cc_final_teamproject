d5aa3cd6d4aa75c3c150cd9acba74ae4f3d50537
samwisethebot <sa.thiounn@gmail.com>
2022-07-04 00:11:10 -0400

    added click interaction placeholder
    item will destroy when clicked

--- cc_group3_final_project/Assets/Scripts/CursorController.cs
+++ cc_group3_final_project/Assets/Scripts/CursorController.cs
@@ -10,12 +10,15 @@ public class CursorController : MonoBehaviour
 
     //Refering to the cursor action map
     private CursorControls controls;
+    
+    private Camera mainCamera;
 
     private void Awake()
     {
         controls = new CursorControls();   
         ChangeCursor(cursor);
         Cursor.lockState = CursorLockMode.Confined;
+        mainCamera = Camera.main;
     }
     
     private void OnEnable()
@@ -42,11 +45,29 @@ public class CursorController : MonoBehaviour
     private void EndedClick()
     {
         ChangeCursor(cursor);
+        DetectObject();
     }
+
+    //Creating a function to know where we're clicking, using raycast from camera to the object
+    private void DetectObject()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(controls.Mouse.Position.ReadValue<Vector2>());
+        RaycastHit hit;
+    
+        if(Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider != null)
+            {
+            IClicked click = hit.collider.gameObject.GetComponent<IClicked>();
+            if (click != null) click.onClickAction();
+            Debug.Log("Hit Collider" + hit.collider.tag);
+            }
+        }      
+    }
+
 
     private void ChangeCursor(Texture2D cursorType)
     {
-        
         Cursor.SetCursor(cursorType, Vector2.zero, CursorMode.Auto);
     }
 
